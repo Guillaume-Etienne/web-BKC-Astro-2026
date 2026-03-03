446a06c7b7f0eb0d02cac0cf576dee5d9075cd77
Guillaume <gsetienne@free.fr>
2026-03-03 19:09:15 +0200

    Avec le .github/workflow V2 ATTENTION .gitignore modifié!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+const isProd = process.env.DEPLOY_TARGET === 'production';
+
 export default defineConfig({
-  site: 'https://www.bilenekite.com',
+  site: isProd ? 'https://www.bilenekite.com' : 'https://guillaume-etienne.github.io',
+  base: isProd ? '/' : '/web-BKC-Astro-2026',
   integrations: [sitemap()],
 });
